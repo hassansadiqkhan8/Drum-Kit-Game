82d19643bc95f88e1b83bdc4b301c6446ab32d39
hassansadiqkhan8 <44670637+hassansadiqkhan8@users.noreply.github.com>
2024-02-25 21:32:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const drumButtons = document.querySelectorAll(".drum")
+
+
+for (let i = 0; i < drumButtons.length; i++) {
+
+    drumButtons[i].addEventListener("click", function() {
+
+        let buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml)
+
+    })
+
+}
+
+document.addEventListener("keypress", function(e){
+    let keyPress = e.key
+    makeSound(keyPress)
+})
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        default:
+            console.log(buttonInnerHtml);
+    }
+}
